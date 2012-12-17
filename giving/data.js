62f68948db75fccec681ad3a340192ef5a9da254
Matthew Scott <matthew.scott@canonical.com>
2012-12-16 17:12:54 -0700

    Added giving vis

---
+++ giving/data.js
@@ -0,0 +1,264 @@
+var data = [
+  {
+    name: "Anthrocon",
+    years: [
+      { 
+        year: 1999,
+        attendance: 845,
+        //charityName: "Great Valley Nature Center",
+        charityAmount: 3600
+      },
+      { 
+        year: 2000,
+        attendance: 1128,
+        //charityName: "National Greyhound Adoption Program",
+        charityAmount: 6500
+      },
+      { 
+        year: 2001,
+        attendance: 1457,
+        //charityName: "Reins of Life",
+        charityAmount: 7273
+      },
+      { 
+        year: 2002,
+        attendance: 1648,
+        //charityName: "Canine Partners for Life",
+        charityAmount: 13280
+      },
+      { 
+        year: 2003,
+        attendance: 1949,
+        //charityName: "Support Our Shelters",
+        charityAmount: 8348
+      },
+      { 
+        year: 2004,
+        attendance: 2404,
+        //charityName: "Forgotten Felines and Fidos",
+        charityAmount: 7200
+      },
+      { 
+        year: 2005,
+        attendance: 2370,
+        //charityName: "Greater Philadelphia Search and Rescue",
+        charityAmount: 6470
+      },
+      { 
+        year: 2006,
+        attendance: 2489,
+        //charityName: "Western Pennsylvania National Wild Animal Orphanage",
+        charityAmount: 8407
+      },
+      { 
+        year: 2007,
+        attendance: 2849,
+        //charityName: "Animal Friends",
+        charityAmount: 7608
+      },
+      { 
+        year: 2008,
+        attendance: 3390,
+        //charityName: "Pittsburgh Parrot Rescue",
+        charityAmount: 13154
+      },
+      { 
+        year: 2009,
+        attendance: 3776,
+        //charityName: "Animal Rescue League Wildlife Rehabilitation Center",
+        charityAmount: 8992
+      },
+      { 
+        year: 2010,
+        attendance: 4238,
+        //charityName: "Fayette Friends of Animals",
+        charityAmount: 12849
+      },
+      { 
+        year: 2011,
+        attendance: 4400,
+        //charityName: "Toonseum",
+        charityAmount: 11522
+      },
+      { 
+        year: 2012,
+        attendance: 5179,
+        //charityName: "Hello Bully",
+        charityAmount: 20656
+      }
+    ]
+  },
+  {
+    name: "Further Confusion",
+    years: [
+      { 
+        year: 1999,
+        attendance: 691,
+        //charityName: "Great Valley Nature Center",
+        charityAmount: 3500
+      },
+      { 
+        year: 2000,
+        attendance: 987,
+        //charityName: "National Greyhound Adoption Program",
+        charityAmount: 6000
+      },
+      { 
+        year: 2001,
+        attendance: 1114,
+        //charityName: "Reins of Life",
+        charityAmount: 7240
+      },
+      { 
+        year: 2002,
+        attendance: 1137,
+        //charityName: "Canine Partners for Life",
+        charityAmount: 5200
+      },
+      { 
+        year: 2003,
+        attendance: 1201,
+        //charityName: "Support Our Shelters",
+        charityAmount: 6000
+      },
+      { 
+        year: 2004,
+        attendance: 1411,
+        //charityName: "Forgotten Felines and Fidos",
+        charityAmount: 10000
+      },
+      { 
+        year: 2005,
+        attendance: 1712,
+        //charityName: "Greater Philadelphia Search and Rescue",
+        charityAmount: 10000
+      },
+      { 
+        year: 2006,
+        attendance: 1911,
+        //charityName: "Western Pennsylvania National Wild Animal Orphanage",
+        charityAmount: 15000
+      },
+      { 
+        year: 2007,
+        attendance: 2062,
+        //charityName: "Animal Friends",
+        charityAmount: 16000
+      },
+      { 
+        year: 2008,
+        attendance: 2423,
+        //charityName: "Pittsburgh Parrot Rescue",
+        charityAmount: 22000
+      },
+      { 
+        year: 2009,
+        attendance: 2587,
+        //charityName: "Animal Rescue League Wildlife Rehabilitation Center",
+        charityAmount: 16000
+      },
+      { 
+        year: 2010,
+        attendance: 2770,
+        //charityName: "Fayette Friends of Animals",
+        charityAmount: 18114
+      },
+      { 
+        year: 2011,
+        attendance: 2801,
+        //charityName: "Toonseum",
+        charityAmount: 8000 //XXX
+      },
+      { 
+        year: 2012,
+        attendance: 3021,
+        //charityName: "Hello Bully",
+        charityAmount: 7000 //XXX
+      }
+    ]
+  },
+  {
+    name: "Midwest Fur Fest",
+    years: [
+      { 
+        year: 2000,
+        attendance: 388,
+        //charityName: "National Greyhound Adoption Program",
+        charityAmount: 3522
+      },
+      { 
+        year: 2001,
+        attendance: 511,
+        //charityName: "Reins of Life",
+        charityAmount: 6800
+      },
+      { 
+        year: 2002,
+        attendance: 685,
+        //charityName: "Canine Partners for Life",
+        charityAmount: 6088
+      },
+      { 
+        year: 2003,
+        attendance: 800,
+        //charityName: "Support Our Shelters",
+        charityAmount: 6500
+      },
+      { 
+        year: 2004,
+        attendance: 959,
+        //charityName: "Forgotten Felines and Fidos",
+        charityAmount: 7000
+      },
+      { 
+        year: 2005,
+        attendance: 1066,
+        //charityName: "Greater Philadelphia Search and Rescue",
+        charityAmount: 6800
+      },
+      { 
+        year: 2006,
+        attendance: 1422,
+        //charityName: "Western Pennsylvania National Wild Animal Orphanage",
+        charityAmount: 13048.96
+      },
+      { 
+        year: 2007,
+        attendance: 1689,
+        //charityName: "Animal Friends",
+        charityAmount: 15193
+      },
+      { 
+        year: 2008,
+        attendance: 1992,
+        //charityName: "Pittsburgh Parrot Rescue",
+        charityAmount: 15000
+      },
+      { 
+        year: 2009,
+        attendance: 2040,
+        //charityName: "Animal Rescue League Wildlife Rehabilitation Center",
+        charityAmount: 12799
+      },
+      { 
+        year: 2010,
+        attendance: 2285,
+        //charityName: "Fayette Friends of Animals",
+        charityAmount: 11300
+      },
+      { 
+        year: 2011,
+        attendance: 2600,
+        //charityName: "Toonseum",
+        charityAmount: 19574.79
+      },
+      { 
+        year: 2012,
+        attendance: 3216,
+        //charityName: "Hello Bully",
+        charityAmount: 40500
+      }
+    ]
+  }
+];
+
