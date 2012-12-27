f675effebe5447714838090ec154433621a2258e
Matthew Scott <mjs@mjs-svc.com>
2012-12-27 08:33:57 -0800

    Licensing and comments

--- microsurvey/microsurvey.js
+++ microsurvey/microsurvey.js
@@ -1,10 +1,22 @@
-var vis = d3.select('#fig')
-    .append('svg')
-    .attr('width', 1600)
-    .attr('height', 1100)
-    .append('g')
-    .attr('transform', 'translate(96,20)');
+/*
+    http://vis.adjectivespecies.com - visualizations of the furry fandom.
+    Copyright (C) 2012  Matthew Scott
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Initialize variables.
 var offsets = [],
     scaleOffsets = [],
     filter = [], filteredCount = 0;
@@ -12,18 +24,33 @@ for (var i = 0; i < 32; i++) {
     offsets.push([0, 0, 0, 0, 0, 0]);
     scaleOffsets.push([0, 0, 0, 0, 0, 0]);
 }
+
+// Filter out empty responses.
 var data = responses.filter(function(d) {
     return !isNaN(parseInt(d[3]));
 });
+
+// Build a list of offsets - each answer's offset is the sum of the answers
+// above it.
 data.forEach(function(response) {
     response.slice(3).forEach(function(answer, i) {
         offsets[i][answer - 1]++;
     });
 });
 
+// Build the SVG.
+var vis = d3.select('#fig')
+    .append('svg')
+    .attr('width', 1600)
+    .attr('height', 1100)
+    .append('g')
+    .attr('transform', 'translate(96,25)');
+
+// Scales for placing data proportionally across the SVG.
 var x = d3.scale.linear().range([0, 1500]).domain([0, 31]),
     y = d3.scale.linear().range([0, 900]).domain([0, data.length - 1]);
 
+// Build the response lines.
 var line = d3.svg.line()
     .x(function(d, i) { return x(i); })
     .y(function(d, i) {
@@ -47,6 +74,8 @@ var path = paths.selectAll('.response')
         return line(d.slice(3));
     });
 
+// Stack layouts use a data format inverted from what we have (arrays of 
+// answers rather than arrays of questions).
 var invertedOffsets = [];
 for (var i = 0; i < 6; i++) {
     var newArray = offsets.map(function(d) { 
@@ -59,11 +88,13 @@ for (var i = 0; i < 6; i++) {
 
 var barsStack = d3.layout.stack();
 
+// Add the question groups.
 var bars = vis.selectAll('.question')
     .data(barsStack(invertedOffsets))
     .enter().append('g')
     .attr('class', 'question');
 
+// Add the answer bars.
 bars.selectAll('.answer')
     .data(function(d) { return d; })
     .enter().append('rect')
@@ -96,6 +127,7 @@ bars.selectAll('.answer')
     .append('title')
     .text(function(d) { return schema[d.x]; });
 
+// Add X-axis labels.
 var labels = vis.append('g');
 labels.selectAll('.qlabel')
     .data(shortnames)
@@ -110,6 +142,7 @@ labels.selectAll('.qlabel')
         return shortnames[i];
     });
 
+// Add Y-axis labels.
 labels.append('text')
     .attr('x', -8)
     .attr('y', 10)
@@ -126,13 +159,18 @@ labels.append('text')
     .attr('text-anchor', 'end')
     .text('Strongly Agree');
 
+// Add a correlation label
 var corr = labels.append('text')
     .attr('x', 1500)
     .attr('y', -10)
     .attr('text-anchor', 'end')
-    .text('Distance from avg.: ')
+    .text('Percentage of respondents (as compared to average): ')
     .append('tspan');
 
+/*
+ * Update the opacity of the response lines according to the current filter.
+ * Additionally, update the percentage of respondents text at the top.
+ */
 function updateFilteredPaths() {
     filteredCount = 0;
     path.classed('filtered', false);
@@ -142,23 +180,55 @@ function updateFilteredPaths() {
 
     if (filteredCount > 0) {
         var avg = responses.length / 6;
-        corr.text(d3.format('+.4g')((filteredCount - avg) / avg))
+        corr.text(
+            d3.format('.4%')(filteredCount / responses.length) + ' (' +
+            d3.format('+.4%')((filteredCount - avg) / responses.length)  + ')'
+        );
     } else {
         corr.text('');
     }
 }
 
+/* 
+ * Test to see if a response fits within the filter by making sure it matches
+ * the answer or answers selected for each question filtered.
+ */
 function filterResponse(d) {
     var passes = filter.length > 0,
-        testVal = d.slice(3);
-    for (var i = 0; i < filter.length; i++ ) {
-        if (testVal[filter[i][0]] !== filter[i][1]) {
-            passes = false;
-            break;
+        testVal = d.slice(3),
+        agg = aggregateFilter();
+
+    for (var i = 0; i < agg.length; i++ ) {
+        if (agg[i]) {
+            var innerPasses = false;
+            for (var j = 0; j < agg[i].length; j++) {
+                if (testVal[i] === agg[i][j]) {
+                    innerPasses = true;
+                    break;
+                }
+            }
+            passes = passes && innerPasses;
         }
     }
+
     if (passes) {
         filteredCount++;
     }
+
     return passes;
+}
+
+/*
+ * Aggregate the filter array into an array of arrays, where the index is the
+ * question, and the value is an array of answers.
+ */
+function aggregateFilter() {
+    var agg = [];
+    for (var i = 0; i < filter.length; i++) {
+        if (!agg[filter[i][0]]) {
+            agg[filter[i][0]] = [];
+        }
+        agg[filter[i][0]].push(filter[i][1]);
+    }
+    return agg;
 }
